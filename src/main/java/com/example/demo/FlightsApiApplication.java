022d64addf93d9802f739e94f1959ebe7e5655b4
FlorIniguez <iniguezflorencia788@gmail.com>
2024-03-02 01:48:17 -0300

    ejercicio hecho

--- src/main/java/com/example/demo/FlightsApiApplication.java
+++ src/main/java/com/example/demo/FlightsApiApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class VuelosApiApplication {
+@RestController
+public class FlightsApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VuelosApiApplication.class, args);
+		SpringApplication.run(FlightsApiApplication.class, args);
 	}
+
+
 
 }
