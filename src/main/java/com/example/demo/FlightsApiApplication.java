a5ae8055c640cc9ddec457a55d3e6780dcd7f2f6
FlorIniguez <iniguezflorencia788@gmail.com>
2024-03-06 10:26:01 -0300

    crud

--- src/main/java/com/example/demo/FlightsApiApplication.java
+++ src/main/java/com/example/demo/FlightsApiApplication.java
@@ -2,10 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class FlightsApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/model/Flight.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String origen;
+    private String destino;
+    private String fechaHoraSalida;
+    private String fechaHoraLLegada;
+    private double precioEnPesos;
+    private String frecuencia;
+
+    public Flight(String origen, String destino, String fechaHoraSalida, String fechaHoraLLegada, double precioEnPesos, String frecuencia) {
+        this.origen = origen;
+        this.destino = destino;
+        this.fechaHoraSalida = fechaHoraSalida;
+        this.fechaHoraLLegada = fechaHoraLLegada;
+        this.precioEnPesos = precioEnPesos;
+        this.frecuencia = frecuencia;
+    }
+}
