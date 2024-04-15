5bd450bec49809f4fe4126b2a2e9e46ded4db886
FlorIniguez <iniguezflorencia788@gmail.com>
2024-04-14 22:05:00 -0300

    arreglando FlightDto

--- src/main/java/com/example/demo/utils/FlightUtils.java
+++ src/main/java/com/example/demo/utils/FlightUtils.java
@@ -13,23 +13,8 @@ import java.util.stream.Collectors;
 
 @Component
 public class FlightUtils {
-    //recibe un vuelo, el precio dolar y devuelve el vuelo con el precio convertido el FlightDTO
-    public FlightDto flightMapper(Flight flight, double price ){
-        return new FlightDto(flight.getId(), flight.getOrigin(), flight.getDestiny(),flight.getDepartureTime(),
-                flight.getArrivingTime(),flight.getPrice() * price, flight.getFrequency(), flight.getCompany());
-    }
 
     public List<FlightDto> flightMapperDto(List <Flight> flights, double price){
-
-       /* List<FlightDto> flightDtos = new ArrayList<>();
-        //recorro la lista de vuelos
-        for(Flight flight : flights) {
-            // por cada vuelta a la lista le agrego un nuevo FlightDTO
-            flightDtos.add(new FlightDto(flight.getId(), flight.getOrigin(), flight.getDestiny(),flight.getDepartureTime(),
-                    flight.getArrivingTime(),flight.getPrice() * price, flight.getFrequency()));
-        }
-        return flightDtos; */
-
         //convierto la lista flights que viene por parametro en flujo
         //la mapeo para que por cada vuelo cree un nuevo flightDTO
         return flights.stream()
